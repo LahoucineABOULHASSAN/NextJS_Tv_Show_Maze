56afa7d69ecc1b30b01b50295f0495912798c4dd
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2021-09-03 18:56:52 +0100

    add filtering results by search name

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -5,7 +5,7 @@ const SearchForm = ({ handleFilter }) => {
       onSubmit={(event) => event.preventDefault()}
     >
       <input
-        className="border border-2 rounded-xl border-gray-500 focus:outline-none focus:border-red-300 py-1 px-3 w-full"
+        className="border border-1 rounded-2xl border-gray-700 focus:outline-none focus:border-green-600 focus:bg-green-50 py-2 px-4 w-full"
         type="search"
         placeholder="Search"
         onChange={(event) => handleFilter(event.target.value)}
--- pages/series/Series.jsx
+++ pages/series/Series.jsx
@@ -1,11 +1,30 @@
-import { Card } from "../../components";
+import { useState } from "react";
+import { Card, SearchForm } from "../../components";
 const Series = ({ shows }) => {
+  const [search, setSearch] = useState("");
+  const handleFilter = (elem) => {
+    const searchItem = elem.toLowerCase();
+    setSearch(searchItem);
+  };
+
+  const alert = (
+    <p className="rounded-2xl w-10/12 text-2xl text-center capitalize bg-red-200 p-4 my-8 mx-auto">
+      this show is not on our list
+    </p>
+  );
+
+  const data = shows
+    .filter((elem) => {
+      return elem.name.toLowerCase().includes(search);
+    })
+    .map((show) => <Card show={show} key={show.id} />);
+
   return (
     <section>
+      <SearchForm handleFilter={handleFilter} />
+      {!data.length && alert}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 w-10/12 my-12 mx-auto">
-        {shows.map((show) => (
-          <Card show={show} key={show.id} />
-        ))}
+        {data.length ? data : null}
       </div>
     </section>
   );
