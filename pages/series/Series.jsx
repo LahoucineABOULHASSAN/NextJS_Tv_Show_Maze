c85ab797ab2c1989108e16a52483da3b3e7e01fa
lahoucine aboulhassan <em.aboulhassan@gmail.com>
2021-09-09 16:15:39 +0100

    last update

--- pages/series/Series.jsx
+++ pages/series/Series.jsx
@@ -47,7 +47,7 @@ const Series = ({ shows }) => {
       {!data.length && alert}
       {shows ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 w-10/12 my-12 mx-auto">
-          {data.length ? data : null}
+          {data ? data : null}
         </div>
       ) : (
         loading
